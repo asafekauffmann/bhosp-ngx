29b14587453ce2e538d30d8788dab77807c1c324
Asafe Kauffmann <asafe.kauffmann@gmail.com>
2021-11-25 11:32:55 -0300

    up

--- src/app/login/toggle-switch/toggle-switch.component.ts
+++ src/app/login/toggle-switch/toggle-switch.component.ts
@@ -1,20 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
-declare const recuperaDarkModeCssEtoggle: any; 
-
 @Component({
    selector: 'app-toggle-switch',
    templateUrl: './toggle-switch.component.html',
    styleUrls: ['./toggle-switch.component.sass']
 })
 export class ToggleSwitchComponent implements OnInit {
-   
-   callFun(): void {
-      recuperaDarkModeCssEtoggle();
-   }  
-   constructor() {
-      
-   }
+
+   constructor() { }
 
    ngOnInit(): void {
    }
