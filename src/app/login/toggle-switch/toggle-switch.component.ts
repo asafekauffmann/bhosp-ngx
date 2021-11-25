f32bc61c3400119cdda5cb26ac77c324d78c090c
Asafe Kauffmann <asafe.kauffmann@gmail.com>
2021-11-25 13:24:09 -0300

    up

--- src/app/login/toggle-switch/toggle-switch.component.ts
+++ src/app/login/toggle-switch/toggle-switch.component.ts
@@ -12,60 +12,57 @@ export class ToggleSwitchComponent implements OnInit {
    estiloAtivo = false;
    indexEstilo = 0;
 
-
    constructor() { 
-
       let styles = document.getElementsByTagName('style')
          for (let index = 0; index < styles.length; index++) {
 
             let styleToCheck = document.getElementsByTagName('style')[index].innerHTML;
                if(styleToCheck.includes(this.estiloComentado)) {
-               const indexEstilo = index;
+               this.indexEstilo = index;
             }
          }
    }
 
    ngOnInit(): void {
+      /* let referenciaAngular = this;
+      const apps: HTMLElement = document.getElementById('darkSwitch') as HTMLElement
+      apps.addEventListener('click', function() {
+         referenciaAngular.recuperaDarkModeCssEtoggle()
+      }) */
+
    }
 
-   recuperaDarkModeCssEtoggle() {
-      if(!this.estiloAtivo) {
+   recuperaDarkModeCssEtoggle(event: any) {
+      event.stopPropagation();
+      console.log(event);
+      if (!this.estiloAtivo) { 
          let darkMode = document.getElementsByTagName('style')[this.indexEstilo].innerHTML
          console.log(darkMode)
          darkMode = darkMode.replace(this.estiloComentado, this.estiloDescomentado)
          document.getElementsByTagName('style')[this.indexEstilo].innerHTML = darkMode
-
          this.estiloAtivo = !this.estiloAtivo;
+         return
+
       } else {
          let darkModeDescomentado = document.getElementsByTagName('style')[this.indexEstilo].innerHTML
          console.log(darkModeDescomentado)
          darkModeDescomentado = darkModeDescomentado.replace(this.estiloDescomentado, this.estiloComentado)
          document.getElementsByTagName('style')[this.indexEstilo].innerHTML = darkModeDescomentado
-
          this.estiloAtivo = !this.estiloAtivo;
+         return 
       }
    }
 
-   adicionaDarkMode(this: any) {
-      const darkMode =  this.recuperaDarkModeCssEtoggle()()
+   /* adicionaDarkMode(this: any) {
+      const darkMode =  this.recuperaDarkModeCssEtoggle()
       this.darkMode = darkMode.replace(this.styleComent, this.styleToAdd)
    }
 
    removeDarkMode(this: any) {
-      let darkMode =  this.threcuperaDarkModeCssEtoggle()()
+      let darkMode =  this.threcuperaDarkModeCssEtoggle()
       darkMode = darkMode.replace(this.styleToAdd, this.styleComent)
-   }
-
-   
-   styleComent(styleToAdd: (styleToAdd: any, styleComent: any) => any, styleComent: any): any { }
-   styleToAdd(styleToAdd: any, styleComent: any): any { }
-
-
-
-   document.getElementById('darkSwitch').addEventListener('click', function() {
-      this.recuperaDarkModeCssEtoggle()
-   })
-}
+   } */
+} 
 
 
 
