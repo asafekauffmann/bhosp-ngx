174f5b6d38c770d06c586d2f222a26140b489e53
Asafe Kauffmann <asafe.kauffmann@gmail.com>
2021-11-29 12:11:36 -0300

    up

--- src/app/shared/dark-mode/dark-mode.component.ts
+++ src/app/shared/dark-mode/dark-mode.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./dark-mode.component.sass']
 })
 export class DarkModeComponent implements OnInit {
+   
    estiloComentado = '/*html, img, video, iframe, button {filter: invert(0.95) hue-rotate(180deg)}*/';
    estiloDescomentado = 'html, img, video, iframe, button {filter: invert(0.95) hue-rotate(180deg)}';
    estiloAtivo = false;
